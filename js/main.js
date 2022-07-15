d34ef1b8db928dc73874a6ecac1240105f88e208
LeoGrasso2 <108083932+LeoGrasso2@users.noreply.github.com>
2022-07-15 14:48:52 -0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -4,11 +4,25 @@ let numNota=0;
 let i=0;
 
 
-function addNota(){
-    let notaTitle=prompt(`Nueva nota.\n----------\nTitulo:`);
-    let notaTexto=prompt(`Nueva nota.\n----------\nTexto:`);
-    this.i=i;
+function addNotaTemp(){
     
+    let notaTitle=  document.getElementById('inputTitle');
+    let notaTexto=  document.getElementById('inputTexto');
+
+    notaTexto.addEventListener()
+    contenedor = document.createElement('div');
+    contenedor.setAttribute("style", "box-shadow: 2px 5px 5px rgba(117, 117, 117, 0.37);  height:fit-content; font-family: sans-serif; border-radius: 10px; margin-right: 10px; max-height: fit-content; display: flex; min-width: 150px; background-color: rgb(238, 255, 0); flex-direction: column; overflow-wrap: break-word;");
+    contenedor.innerHTML= `<div class="notaTop"><div class="tituloNota"><u><textarea placeholder="Ingrese un titulo" class="inputTitle" name="inputTitle" id="notaTitle"></textarea></u></div></div><br><div class="contenidoNota" style="align-items:center"><textarea placeholder="Ingrese su nota" class="inputTexto" id="inputTexto"></textarea>`;
+    document.getElementById("notasTotales").appendChild(contenedor);
+    
+    
+}
+
+function addNota(){
+    let notaTitle=  document.getElementById('inputTitle');
+    let notaTexto=  document.getElementById('inputTexto');
+
+    this.i=i; 
 
     listaNotas.push({
         notaTitle: notaTitle,
@@ -19,11 +33,12 @@ function addNota(){
     contenedor = document.createElement('div');
     contenedor.setAttribute("class", `nota`);
     contenedor.setAttribute("id", `nota${listaNotas[i].numNota}`);
-    contenedor.innerHTML= `<div class="tituloNota"><u><b>${notaTitle}</b></u><button class="botonDelete" onclick="deleteNota(${listaNotas[i].numNota})">X</button></div><br><div class="contenidoNota">${notaTexto}`;
+    contenedor.setAttribute("style", "box-shadow: 2px 5px 5px rgba(117, 117, 117, 0.37);  height:fit-content; font-family: sans-serif; border-radius: 10px; margin-right: 10px; max-width: 250px; max-height: fit-content; display: flex; min-width: 150px; background-color: rgb(238, 255, 0); flex-direction: column; overflow-wrap: break-word;");
+    
+    contenedor.innerHTML= `<div class="notaTop"><div class="tituloNota"><u><b>${notaTitle}</b></u></div><button class="botonDelete" onclick="deleteNota(${listaNotas[i].numNota})">X</button></div><br><div class="contenidoNota"><div class="notaTexto">${notaTexto}</div><button class="botonEdit" onclick="editarNota(${listaNotas[i].numNota})">Editar</button>`;
     document.getElementById("notasTotales").appendChild(contenedor);
     i++;
 }
-
 function deleteNota(del){
     let borrarNota = document.getElementById(`nota${del}`);
     borrarNota.remove();
@@ -38,4 +53,24 @@ function deleteNota(del){
     if(i<0){
         i=0;
     }
+}
+
+function editarNota(del){
+    let notaTitle=prompt(`Editar nota.\n----------\nTitulo:`);
+    let notaTexto=prompt(`Editar nota.\n----------\nTexto:`);
+    this.i=i;
+    
+    let editNota = document.getElementById(`nota${del}`);
+
+    
+
+    listaNotas.splice(del, {
+        notaTitle: notaTitle,
+        notaTexto: notaTexto,
+        numNota: del,
+    })
+    
+    editNota.setAttribute("style", "box-shadow: 2px 5px 5px rgba(117, 117, 117, 0.37); font-family: sans-serif; border-radius: 10px; margin-right: 10px; max-width: 250px; max-height: 550px; height:fit-content; display: flex; min-width: 150px; background-color: rgb(238, 255, 0); flex-direction: column; overflow-wrap: break-word;");
+
+    editNota.innerHTML = `<div class="notaTop"><div class="tituloNota"><u><b>${notaTitle}</b></u></div><button class="botonDelete" onclick="deleteNota(${del})">X</button></div><br><div class="contenidoNota"><div class="notaTexto">${notaTexto}</div><div class="footerNota"><button class="botonEdit" onclick="editarNota(${del})">Editar</button></div></div>`;
 }
